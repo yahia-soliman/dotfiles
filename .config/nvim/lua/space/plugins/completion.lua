e545feae6b1cc19e9fe18094d0d1870e2087cff3
Yahia <yn7599@gmail.com>
2025-01-07 11:37:45 +0200

    [UPD] nvim: snippets

--- .config/nvim/lua/space/plugins/completion.lua
+++ .config/nvim/lua/space/plugins/completion.lua
@@ -18,12 +18,15 @@ return { -- Autocompletion
 				-- `friendly-snippets` contains a variety of premade snippets.
 				--    See the README about individual language/framework/plugin snippets:
 				--    https://github.com/rafamadriz/friendly-snippets
-				-- {
-				--   'rafamadriz/friendly-snippets',
-				--   config = function()
-				--     require('luasnip.loaders.from_vscode').lazy_load()
-				--   end,
-				-- },
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+						require("luasnip.loaders.from_vscode").lazy_load({
+							paths = { vim.fn.stdpath("config") .. "/lua/space/snippets" },
+						})
+					end,
+				},
 			},
 		},
 		"saadparwaiz1/cmp_luasnip",
