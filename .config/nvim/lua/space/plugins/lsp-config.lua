f7fe3859899c0131c33769601b245dfac77af4db
Yahia <yn7599@gmail.com>
2025-10-25 19:46:57 +0300

    nvim: config astro lsp

--- .config/nvim/lua/space/plugins/lsp-config.lua
+++ .config/nvim/lua/space/plugins/lsp-config.lua
@@ -199,6 +199,7 @@ return { -- LSP Configuration & Plugins
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"eslint",
+			"astro",
 			"cssls",
 			"tailwindcss",
 		})
